00e5f146e72d99b41476a095ae4eedc4523fa2c6
Arun Kumar <arunkumar7617@gmail.com>
2020-02-21 03:31:15 +0400

    Add rental component, rental list component, rental detail and rental service

---
+++ src/app/rental/rental-detail/rental-detail.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RentalService } from '../shared/rental.service';
+import { Rental } from '../shared/rental.model';
+
+@Component({
+    selector: 'app-rental-detail',
+    templateUrl: './rental-detail.component.html',
+    styleUrls: ['./rental-detail.component.scss'],
+})
+export class RentalDetailComponent implements OnInit {
+    constructor(
+        private route: ActivatedRoute,
+        private rentalService: RentalService
+    ) {}
+    currentId: string;
+    rental: Rental;
+    ngOnInit() {
+        this.route.params.subscribe(params => {
+            this.getRental(params['rentalId']);
+        });
+    }
+
+    getRental(rentalId: string) {
+        this.rentalService
+            .getRentalById(rentalId)
+            .subscribe((rental: Rental) => {
+                this.rental = rental;
+            });
+    }
+}
